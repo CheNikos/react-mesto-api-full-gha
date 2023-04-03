c8d8ba35f958465ce80ca9229abef0a4cba6b55e
CheNikos <chenikos@yandex.ru>
2023-04-03 22:47:29 +0300

    изменил порты

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://api.mesto.parkharidi.nomoredomains.work";
+export const BASE_URL = "https://api.mesto.parkharidi.nomoredomains.work";
 
 const getResponse = (res) => {
   return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
